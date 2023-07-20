62e9cda40a0fc3f77e3126233b5f1d5c70c40d9c
Matome <seletematome3@gmail.com>
2023-07-20 11:22:12 +0200

    first commit

---
+++ src/main/java/com/springbootuploadcsvfile/SpringBootUploadCSVfile/SpringBootUploadCsVfileApplication.java
@@ -0,0 +1,13 @@
+package com.springbootuploadcsvfile.SpringBootUploadCSVfile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootUploadCsVfileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootUploadCsVfileApplication.class, args);
+	}
+
+}
