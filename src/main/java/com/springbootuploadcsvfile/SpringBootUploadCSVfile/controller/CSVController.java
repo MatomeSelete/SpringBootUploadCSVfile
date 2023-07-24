a0bf6fff588eb2a1f5d630a1f0f68b5bda8f0288
Matome <seletematome3@gmail.com>
2023-07-24 15:27:29 +0200

    fixing this error Compilation failure [ERROR] No compiler is provided in this environment. Perhaps you are running on a JRE rather than a JDK?

--- src/main/java/com/springbootuploadcsvfile/SpringBootUploadCSVfile/controller/CSVController.java
+++ src/main/java/com/springbootuploadcsvfile/SpringBootUploadCSVfile/controller/CSVController.java
@@ -1,10 +1,12 @@
 package com.springbootuploadcsvfile.SpringBootUploadCSVfile.controller;
 
 import com.springbootuploadcsvfile.SpringBootUploadCSVfile.service.CSVService;
-import jdk.internal.loader.Resource;
+//import jdk.internal.loader.Resource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.InputStreamResource;
+import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
